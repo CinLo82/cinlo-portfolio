0749895d1b6fbdf7bbe803320ca24130c7653c8e
Cintia Losada <cinlos3382@gmail.com>
2024-10-24 17:15:35 -0300

    cambio de icon en el menu-blog

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -9,7 +9,7 @@ export const mainMenu = [
 export const menuBlog = [
      {link:"/", content:"Inicio", icon:"lucide:home"},
      {link:"/#about", content:"Sobre mi", icon:"octicon:person-24"},
-     {link:"/#skills", content:"Habilidades", icon:"octicon:person-24"},
+     {link:"/#skills", content:"Habilidades", icon:"mdi:tools"},
      {link:"/#portafolio", content:"Proyectos", icon:"carbon:portfolio"},
      {link:"/#blog", content:"Artículos", icon:"mdi:blog"},
 ]
